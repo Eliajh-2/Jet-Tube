1b706d746319c602c1e3a29c80f6fa084549c921
Hii <elinix1983@icloud.com>
2022-10-29 00:12:55 +0000

    hi

--- src/components/VideoPlay.vue
+++ src/components/VideoPlay.vue
@@ -290,7 +290,7 @@ let videos = [
     title: "18-core iMac Pro Review: Not a Trap!",
     thumbnail:
       "https://i.ytimg.com/vi/jn9mHzXJIV0/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAvJvk4k_UNB9nst4pFP-txM1TLZA",
-    youtubeURL: "https://www.youtube.com/embed/jn9mHzXJIV0",
+    youtubeURL: "https://www.youtube-nocookie.com/embed/spADkxbfuYk?undefined&autoplay=1",
     creator: "Marques Brownlee",
     likes: 0,
     dislikes: 0,
